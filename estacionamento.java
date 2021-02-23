be222d7b7055b7e7cffab1b62c115a72acfac96a
Aline <alinedutratorres@gmail.com>
2021-02-23 17:42:36 -0300

    Alteracao classe estacionamento

--- estacionamento.java
+++ estacionamento.java
@@ -2,16 +2,18 @@ import java.util.Hashtable;
 
 public class Estacionamento {
 
-  public Hashtable<String, String> veiculos;
+  private Hashtable<String,String> veiculos;
 
   public Estacionamento() {
     veiculos = new Hashtable<String, String>();
   }
 
+  public void estaciona(String placa, String veiculo) {
+    veiculos.put(placa, veiculo);
+  }
+
   public static void main(String[] args) {
     Estacionamento e = new Estacionamento();
-    e.veiculos.put("TCP-7030", "Uno");
-    e.veiculos.put("BNF-4501", "Gol");
-    e.veiculos.put("JKL-3481", "Corsa");
+    e.estaciona("GVE-3003", "Palio");
   }
 }
